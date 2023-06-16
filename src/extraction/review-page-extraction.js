47165be6050c1aabb9f197df65200244266549cc
amzgrouplab <amzgrouplab@gmail.com>
2023-06-16 02:38:23 -0400

    Send message to the Slack channel complete

--- src/extraction/review-page-extraction.js
+++ src/extraction/review-page-extraction.js
@@ -45,14 +45,14 @@ module.exports.extractReviews = async (page) => {
         const reviewBlocks = $('.c-review-block');
         const today = new Date();
         const yesterday = new Date(today);
-        yesterday.setDate(yesterday.getDate() - 1);
+        yesterday.setDate(yesterday.getDate() - 15);
         const reviews = $.map(reviewBlocks, (el) => {
             // const dateMatches = $(el).find('.c-review-block__date').text().trim()
             //     .match(/([\d]{1,2}(.)+[\d]{4})/gi);
             const dateMatches = $(el).find('.c-review-block__date').text().trim();
             const datePortion = dateMatches.split(': ')[1]; // Extract the date portion after the colon
             const dateObject = new Date(Date.parse(datePortion));
-            if (dateObject == yesterday) {
+            if (dateObject >= yesterday && dateObject <= today) {
                 const reviewTexts = extractReviewTexts(el);
 
                 const review = {
