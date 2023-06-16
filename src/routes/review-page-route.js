541d7352b20e107087e0adb9261e06a38fdd980b
amzgrouplab <amzgrouplab@gmail.com>
2023-06-16 10:06:38 -0400

    final version 1.0

--- src/routes/review-page-route.js
+++ src/routes/review-page-route.js
@@ -72,7 +72,7 @@ module.exports.handleReviewPage = async (context, globalContext) => {
 
     let reviews = await extractReviews(page);
 
-    const token = "xoxb-5086549358049-5439419378340-NHrVpaMknJIVCJC0hyQ6DYJe";
+    const token = "xoxb-5086549358049-5439419378340-7OYZXEemTmvm6YunucCgZJBu";
     const slackChannel= "reviews";
     //const token = "xoxb-5384634643063-5429247221827-IagETGnAj99DUVEQmdI5a4W5";
     //const slackChannel= "project";
